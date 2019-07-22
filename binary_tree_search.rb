04e5130eb06e94bf9a982fcf7c7640a32f2acc7d
morp <rutesicm27@gmail.com>
2019-07-22 19:27:32 +0200

    Add recursive dfc

--- binary_tree_search.rb
+++ binary_tree_search.rb
@@ -7,6 +7,11 @@ class Node
     @right = nil
   end
 
+  def self.build_tree(ary)
+    @root = Node.new(ary.shift)
+    ary.each {|x| @root.insert(x) }
+    @root
+  end
 
   def insert(v)
     if v < self.value
@@ -24,15 +29,10 @@ class Node
     right ? self.right.insert(v) : self.right = Node.new(v)
   end
 
-  def self.build_tree(ary)
-    @root = Node.new(ary.shift)
-    ary.each {|x| @root.insert(x) }
-    @root
-  end
 
   def breadth_first_search(v)
     return self if self.value == v
-    que = [self]
+    que = [self] 
     until que.empty?
       node = que.shift
       return node if node.value == v
@@ -52,29 +52,27 @@ class Node
     end
   end
 
-
-  def rec_dfs(node, v)
+  def  rec_dfs(v, node = self)
+    return if node.nil?
     return node if node.value == v
-
-    left = rec_dfs(node.left, v) if node.child_left
-    right = rec.dfs(node.right, v) if node.child_right
-    left || right
+    rec_dfs(v, node.left) if node.left
+    rec_dfs(v, node.right) if node.right
   end
 
   def inspect
     "{#{value}::#{left.inspect}|#{right.inspect}}"
   end
 
-  # def to_s
-  #   "Value : #{self.value}"
-  # end
+  def to_s
+    "Value : #{self.value}"
+  end
 
 end
 
 a = [1,2,3,4,5,6,7,8] 
 
 tree = Node.build_tree(a)  # => {1::nil|{2::nil|{3::nil|{4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}}}}
-
+tree.class   # => Node
 treenew = tree.breadth_first_search(2)   # => {2::nil|{3::nil|{4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}}}
 
 puts treenew
@@ -83,8 +81,7 @@ tree.inspect   # => "{1::nil|{2::nil|{3::nil|{4::nil|{5::nil|{6::nil|{7::nil|{8:
 
 puts tree
 
-# >> Ojha
-# >> #<Node:0x0000558d219f1218>
-# >> #<Node:0x0000558d219f11f0>
-# >> #<Node:0x0000558d219f1218>
+tree.rec_dfs(4)   # => {4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}
 
+# >> Value : 2
+# >> Value : 1
