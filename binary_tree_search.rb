4ddbe3daff5451317301c40a99c10b1e114c1152
morp <rutesicm27@gmail.com>
2019-07-22 17:24:59 +0200

    Add bfs def

--- binary_tree_search.rb
+++ binary_tree_search.rb
@@ -1,59 +1,69 @@
 class Node
-  
-  attr_reader :value
-  attr_accessor :left, :right
+  attr_accessor :value, :left, :right, :root
 
-  def initialize(v)
-    @value = v
+  def initialize(val)
+    @value = val
+    @left = nil
+    @right = nil
   end
 
+
   def insert(v)
-    case value <=> v
-    when 1 then insert_left(v) # ~> NoMethodError: undefined method `insert' for nil:NilClass
-    when -1 then insert_right(v)
-    when 0 then false # already present
+    if v < self.value
+      insert_left(v)
+    elsif v >= self.value
+      insert_right(v)
     end
+  end
+
+  def insert_left(v)
+    left ? self.left.insert(v) : self.left = Node.new(v)
+  end
+
+  def insert_right(v)
+    right ? self.right.insert(v) : self.right = Node.new(v)
+  end
+
+  def self.build_tree(ary)
+    @root = Node.new(ary.shift)
+    ary.each {|x| @root.insert(x) }
+    @root
+  end
+
+  def breadth_first_search(v)
+    return self if self.value == v
+    que = [self]
+    until que.empty?
+      node = que.shift
+      return node if node.value == v
+      que << node.right if node.right
+      que << node.left  if node.left
+    end
+    return nil
   end
 
   def inspect
     "{#{value}::#{left.inspect}|#{right.inspect}}"
   end
 
-  private
-
-  def insert_left(v)
-    if left
-      left.insert(v)
-    else
-      self.left = Node.new(v)
-    end
-  end
-
-  def insert_right(v)
-    if right
-      right.insert(v)
-    else
-      self.right = Node.new(v)
-    end
+  def to_s
+    "Value : #{self.value}"
   end
 
 end
 
+a = [1,2,3,4,5,6,7,8] 
 
-tree = Node.new(10)    # => {10::nil|nil}
-tree.left = Node.new(5)
-tree.right = Node.new(15)
+tree = Node.build_tree(a)  # => {1::nil|{2::nil|{3::nil|{4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}}}}
 
-p tree
+treenew = tree.breadth_first_search(4)   # => {4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}
 
+p treenew
 
-tree.insert(3) 
+tree.inspect   # => "{1::nil|{2::nil|{3::nil|{4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}}}}"
 
-# >> {10::{5::nil|nil}|{15::nil|nil}}
+puts tree
 
-# ~> NoMethodError
-# ~> undefined method `insert' for nil:NilClass
-# ~>
-# ~> binary_tree_search.rb:12:in `insert'
-# ~> binary_tree_search.rb:12:in `insert'
-# ~> binary_tree_search.rb:50:in `<main>'
+# >> {4::nil|{5::nil|{6::nil|{7::nil|{8::nil|nil}}}}}
+# >> #<Node:0x000055f7e9c9db28>
+
